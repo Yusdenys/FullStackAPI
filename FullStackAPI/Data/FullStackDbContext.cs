a2f014f4c650c0c946c3f77b55150131e719a1fe
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-28 10:33:32 -0400

    First commit. Creation of the project, models and connection to database.

---
+++ FullStackAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
