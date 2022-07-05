09f26a772c4d7a0cab9f9b2c0a2a530521069337
Yusdenys Perez <yudemendoza@gmail.com>
2022-07-05 19:08:41 -0400

    Has been developed UpdateEmployee and DeleteEmployed method inside EmployeeController

--- FullStackAPI/Controllers/EmployeesController.cs
+++ FullStackAPI/Controllers/EmployeesController.cs
@@ -45,5 +45,43 @@ namespace FullStackAPI.Controllers
             return Ok(employee);
         }
 
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee upemployee)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = upemployee.Name;
+            employee.Email = upemployee.Email;
+            employee.Salary = upemployee.Salary;
+            employee.Phone = upemployee.Phone;
+            employee.Department = upemployee.Department;
+
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id) 
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+
+            _fullStackDbContext.Employees.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
     }
 }
